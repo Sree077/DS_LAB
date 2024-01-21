28cd7e9256e0ccbb266611f26b1c39e40750e636
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2024-01-21 20:35:58 +0530

    added circular-queue

---
+++ cir_queue.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+void main(){
+    int ar[5],ch,i,front=-1,rear=-1,item;
+    do{
+        printf("Choices\n");
+        printf("1)Enque\n");
+        printf("2)Deque\n");
+        printf("3)Display\n");
+        printf("4)Exit\n");
+        printf("Enter choice: ");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:
+                if (front==(rear+1)%5){
+                    printf("Overflow\n");
+                }
+                else if(rear==-1 && front ==-1){
+                    printf("Enter element: ");
+                    scanf("%d",&item);
+                    front=rear=0;
+                    ar[rear]=item;
+                }
+                else{
+                    printf("Enter element: ");
+                    scanf("%d",&item);
+                    rear=(rear+1)%5;
+                    ar[rear]=item;
+                }
+                break;
+            case 2:
+                if (front==-1){
+                    printf("Underflow\n");
+                }
+                else if(front==rear){
+                    printf("Element %d removed\n",ar[front]);
+                    front=rear=-1;
+                }
+                else{
+                    printf("element %d removed\n",ar[front]);
+                    front++;
+                }
+                break;
+            case 3:
+                printf("Elements are: ");
+                for (i=front;i<=rear;i++){
+                    printf("\n%d",ar[i]);
+                }
+                printf("\n");
+                break;
+        }
+    }while(ch!=4);
+}
