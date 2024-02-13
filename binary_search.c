4c1bf9753290ae084abd5dbcdd660018bfbd02dc
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2024-02-13 19:12:50 +0530

    modified

--- binary_search.c
+++ binary_search.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 void main(){
-    
-    int ar[50],first,last,mid,size,num,flag=0;
-    printf("Enter size: ");
+    int ar[50],i,size,item,pos=-1,left,right,mid;
+    printf("Enter size of the array: ");
     scanf("%d",&size);
-    printf("Enter elements(in assending order): ");
-    for (int i=0;i<size;i++){
+    printf("Enter elements: ");
+    for (i=0;i<size;i++){
         scanf("%d",&ar[i]);
     }
+    left = 0;
+    right = size-1;
     printf("Enter element to search: ");
-    scanf("%d",&num);
-    first=0;
-    last=size-1;
-    while(first<=last){
-        mid=first+(last-first)/2;
-        if(ar[mid]==num){
-            printf("Element found\n");
-            flag=1;
+    scanf("%d",&item);
+    while (left<=right){
+        mid = left + (right-left)/2;
+        if (ar[mid]<item){
+            left = mid + 1;
+        }
+        else if (ar[mid]> item){
+            right = mid - 1;
+        }
+        else{
+            pos = mid;
             break;
         }
-        else if(ar[mid]<num){
-            first=mid+1;
-        } 
-        else if(ar[mid]>num){
-            last=mid-1;
-        }
-        
     }
-    if(flag==0){
-        printf("Element not found\n");
+    if (pos == -1){
+        printf("Element not found!!!");
     }
-
+    else{
+        printf("Element found at %d position",pos+1);
+    }
 }
