b29a24e184f4314b0f80d1e4ba4d503dbbc5705f
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2024-02-13 14:40:42 +0530

    modified cir-Queue

--- cir_queue.c
+++ cir_queue.c
@@ -1,52 +1,59 @@
 #include <stdio.h>
 void main(){
-    int ar[5],ch,i,front=-1,rear=-1,item;
+    int ar[5],i,rear=-1,front=-1,ch,item;
     do{
-        printf("Choices\n");
-        printf("1)Enque\n");
-        printf("2)Deque\n");
-        printf("3)Display\n");
+        printf("Choices: \n");
+        printf("1)Enque.\n");
+        printf("2)Deque.\n");
+        printf("3)Display.\n");
         printf("4)Exit\n");
-        printf("Enter choice: ");
+        printf("Enter your choice: ");
         scanf("%d",&ch);
         switch(ch){
             case 1:
-                if (front==(rear+1)%5){
-                    printf("Overflow\n");
-                }
-                else if(rear==-1 && front ==-1){
-                    printf("Enter element: ");
-                    scanf("%d",&item);
-                    front=rear=0;
-                    ar[rear]=item;
-                }
-                else{
-                    printf("Enter element: ");
-                    scanf("%d",&item);
-                    rear=(rear+1)%5;
-                    ar[rear]=item;
-                }
-                break;
+                    if(front==(rear+1)%5){
+                        printf("Overflow!!!\n");
+                    }
+                    else if(rear == -1 && front == -1){
+                        printf("Enter element: ");
+                        scanf("%d",&item);
+                        front = rear = 0;
+                        ar[rear]= item;
+                    }
+                    else{
+                        printf("Enter element: ");
+                        scanf("%d",&item);
+                        rear = (rear+1)%5;
+                        ar[rear]= item;
+                    }
+                    break;
             case 2:
-                if (front==-1){
-                    printf("Underflow\n");
-                }
-                else if(front==rear){
-                    printf("Element %d removed\n",ar[front]);
-                    front=rear=-1;
-                }
-                else{
-                    printf("element %d removed\n",ar[front]);
-                    front++;
-                }
-                break;
+                    if (front == -1){
+                        printf("Underflow!!!\n");
+                    }
+                    else if(front == rear){
+                        printf("Element deleted is: %d\n",ar[front]);
+                        front = rear = -1;
+                    }
+                    else{
+                        printf("Element deleted is: %d\n",ar[front]);
+                        front = (front+1)%5;
+                    }
+                    break;
             case 3:
-                printf("Elements are: ");
-                for (i=front;i<=rear;i++){
-                    printf("\n%d",ar[i]);
-                }
-                printf("\n");
-                break;
+                    if(front == -1) {
+                        printf("Queue is empty.\n");
+                    } 
+                    else {
+                        printf("Elements are: \n");
+                        for(i = front; i != rear; i = (i + 1) % 5) {
+                            printf("%d\n", ar[i]);
+                        }
+                        printf("%d\n", ar[i]); // Print the last element
+                    }
+
+                    break;
+
         }
-    }while(ch!=4);
+    }while(ch != 4);
 }
