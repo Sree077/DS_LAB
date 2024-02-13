cb785ca4e26a94ce8dd77b10284790047f00cfa0
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2024-02-13 15:16:09 +0530

    fixed errors in dequeue.c

--- DEQueue.c
+++ DEQueue.c
@@ -1,84 +1,83 @@
 #include <stdio.h>
-void main(){
-    int ar[10],ch,i,front=-1,rear=-1,item;
-    do{
+
+#define SIZE 10 // Define the size of the deque
+
+void main() {
+    int ar[SIZE], ch, i, front = -1, rear = -1, item;
+    do {
         printf("Choices\n");
-        printf("1)Insert Right\n");
-        printf("2)Delete Left\n");
-        printf("3)Insert Left\n");
-        printf("4)Delete Right\n");
-        printf("5)Display\n");
-        printf("6)Exit\n");
+        printf("1) Insert Right\n");
+        printf("2) Delete Left\n");
+        printf("3) Insert Left\n");
+        printf("4) Delete Right\n");
+        printf("5) Display\n");
+        printf("6) Exit\n");
         printf("Enter choice: ");
-        scanf("%d",&ch);
-        switch(ch){
-            case 1:
-                if (rear==9){
+        scanf("%d", &ch);
+        switch(ch) {
+            case 1: // Insert Right
+                if ((rear + 1) % SIZE == front) {
                     printf("Overflow\n");
-                }
-                else if(rear==-1){
+                } else {
                     printf("Enter element: ");
-                    scanf("%d",&item);
-                    front=rear=0;
-                    ar[rear]=item;
-                }
-                else{
-                    printf("Enter element: ");
-                    scanf("%d",&item);
-                    rear++;
-                    ar[rear]=item;
+                    scanf("%d", &item);
+                    if (rear == -1) { // Empty deque
+                        front = rear = 0;
+                    } else {
+                        rear = (rear + 1) % SIZE;
+                    }
+                    ar[rear] = item;
                 }
                 break;
-            case 2:
-                if (front==-1){
+            case 2: // Delete Left
+                if (front == -1) {
                     printf("Underflow\n");
-                }
-                else if(front==rear){
-                    printf("Element %d removed\n",ar[front]);
-                    front=rear=-1;
-                }
-                else{
-                    printf("element %d removed\n",ar[front]);
-                    front++;
+                } else {
+                    printf("Element %d removed\n", ar[front]);
+                    if (front == rear) { // Only one element was present
+                        front = rear = -1;
+                    } else {
+                        front = (front + 1) % SIZE;
+                    }
                 }
                 break;
-            case 3:
-                if (front==0){
+            case 3: // Insert Left
+                if ((rear + 1) % SIZE == front) {
                     printf("Overflow!!!\n");
-                }
-                else if(front==-1){
+                } else {
                     printf("Enter element: ");
-                    scanf("%d",&item);
-                    front++;
-                    ar[front]=item;
-                }
-                else{
-                    printf("Enter element: ");
-                    scanf("%d",&item);
-                    front--;
-                    ar[front]=item;
+                    scanf("%d", &item);
+                    if (front == -1) { // Empty deque
+                        front = rear = 0;
+                    } else {
+                        front = (front - 1 + SIZE) % SIZE;
+                    }
+                    ar[front] = item;
                 }
                 break;
-            case 4:
-                if (rear==-1){
+            case 4: // Delete Right
+                if (rear == -1) {
                     printf("Underflow\n");
-                }
-                else if(front==rear){
-                    printf("Deleted element is %d\n",ar[rear]);
-                    front=rear=-1;
-                }
-                else{
-                    printf("Deleted element is %d\n",ar[rear]);
-                    rear--;
+                } else {
+                    printf("Deleted element is %d\n", ar[rear]);
+                    if (front == rear) { // Only one element was present
+                        front = rear = -1;
+                    } else {
+                        rear = (rear - 1 + SIZE) % SIZE;
+                    }
                 }
                 break;
-            case 5:
-                printf("Elements are: ");
-                for (i=front;i<=rear;i++){
-                    printf("\n%d",ar[i]);
+            case 5: // Display
+                if (front == -1) {
+                    printf("Deque is empty\n");
+                } else {
+                    printf("Elements are: ");
+                    for (i = front; i != rear; i = (i + 1) % SIZE) {
+                        printf("%d ", ar[i]);
+                    }
+                    printf("%d\n", ar[i]); // Print the last element
                 }
-                printf("\n");
                 break;
         }
-    }while(ch!=6);
+    } while(ch != 6);
 }
